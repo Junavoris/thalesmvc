b66c9325829f89cddfc860837cc15fc0693712d4
Junavoris <sebastian_dia@hotmail.com>
2024-08-15 22:10:24 -0500

    Spring boot project

---
+++ src/main/java/com/thalesTest/thalesmvc/ThalesmvcApplication.java
@@ -0,0 +1,13 @@
+package com.thalesTest.thalesmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThalesmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThalesmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thalesTest/thalesmvc/ThalesmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalesTest.thalesmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThalesmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
